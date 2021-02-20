9dd6ec88bc7f8ddbadc68523bf53754124c64ebd
Dmitriy Sukhov <dstujev@gmail.com>
2021-02-20 21:44:57 +0300

    initial commit

---
+++ precache-manifest.d646e0048f87f6b68616934c9ebde879.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9bee97f3d3bd1471c838d2635dbfb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "400195e2516767d29817",
+    "url": "/static/css/main.444c6ba1.chunk.css"
+  },
+  {
+    "revision": "b5d57739875cee594d37",
+    "url": "/static/js/2.9be9e019.chunk.js"
+  },
+  {
+    "revision": "42065400857fda823701d4a875c4a1a0",
+    "url": "/static/js/2.9be9e019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400195e2516767d29817",
+    "url": "/static/js/main.75fbc80c.chunk.js"
+  },
+  {
+    "revision": "efc3d258f811ff8432aa",
+    "url": "/static/js/runtime-main.a7203f9c.js"
+  },
+  {
+    "revision": "0aaedf2224a98a8fa4caf289ce5b66a6",
+    "url": "/static/media/chart.0aaedf22.svg"
+  },
+  {
+    "revision": "49986e25d72eef749cd78e27f8ebd878",
+    "url": "/static/media/table.49986e25.svg"
+  }
+]);
